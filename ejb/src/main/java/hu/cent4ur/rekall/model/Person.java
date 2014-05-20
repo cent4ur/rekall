984cfdd6636d28a9697a76c6d0578331bdf74091
cent4ur <don.sasha@gmail.com>
2014-05-20 11:35:07 +0200

    Added interests.

--- ejb/src/main/java/hu/cent4ur/rekall/model/Person.java
+++ ejb/src/main/java/hu/cent4ur/rekall/model/Person.java
@@ -7,12 +7,18 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * Represents a person who can log in to the system and make modifications.
@@ -23,27 +29,36 @@ import javax.persistence.TemporalType;
 @NamedQueries({
         @NamedQuery(
             name = "Person.find",
-            query = "SELECT p FROM Person p WHERE p.name = :name AND p.password = :password") })
+            query = "SELECT p FROM Person p WHERE p.name = :name AND p.password = :password"),
+})
 public class Person extends EntityBase implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @NotNull
+    @Size(min = 1, max = 25)
+    @Pattern(regexp = "[^0-9]*", message = "Must not contain numbers")
     private String name;
 
+    @NotNull
+    @NotEmpty
     private String password;
 
-    private Integer experiencePoint;
-
+    @NotNull
     @Temporal(TemporalType.DATE)
+    @Column(name = "registration_date")
     private Date registrationDate;
 
     private String about;
 
-    @OneToMany
-    private List<Friend> markedUsers;
+    @OneToMany(mappedBy = "person")
+    private List<Interest> interests;
 
-    @OneToMany
-    private List<Friend> acceptedUsers;
+    @OneToMany(mappedBy = "marker")
+    private List<Friendship> markedUsers;
+
+    @OneToMany(mappedBy = "accepter")
+    private List<Friendship> acceptedUsers;
 
     public Person() {
     }
@@ -69,14 +84,6 @@ public class Person extends EntityBase implements Serializable {
         this.password = password;
     }
 
-    public Integer getExperiencePoint() {
-        return experiencePoint;
-    }
-
-    public void setExperiencePoint(Integer experiencePoint) {
-        this.experiencePoint = experiencePoint;
-    }
-
     public Date getRegistrationDate() {
         return registrationDate;
     }
@@ -91,5 +98,29 @@ public class Person extends EntityBase implements Serializable {
 
     public void setAbout(String about) {
         this.about = about;
+    }
+
+    public List<Interest> getInterests() {
+        return interests;
+    }
+
+    public void setInterests(List<Interest> interests) {
+        this.interests = interests;
+    }
+
+    public List<Friendship> getMarkedUsers() {
+        return markedUsers;
+    }
+
+    public void setMarkedUsers(List<Friendship> markedUsers) {
+        this.markedUsers = markedUsers;
+    }
+
+    public List<Friendship> getAcceptedUsers() {
+        return acceptedUsers;
+    }
+
+    public void setAcceptedUsers(List<Friendship> acceptedUsers) {
+        this.acceptedUsers = acceptedUsers;
     }
 }
