b2082fa442d1c7f3f79c39db3e326a6214e77b25
cent4ur <don.sasha@gmail.com>
2014-05-20 11:12:56 +0200

    Initial version.

---
+++ ejb/src/main/java/hu/cent4ur/rekall/model/Tag.java
@@ -0,0 +1,36 @@
+// Copyright (c) 2014, the Rekall project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package hu.cent4ur.rekall.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@XmlRootElement
+public class Tag extends EntityBase implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
